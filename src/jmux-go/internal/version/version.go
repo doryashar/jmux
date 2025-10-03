32b1a80c2258d1af3772b29d1f52f2af8c3e1060
doryashar <dor.yashar@gmail.com>
2025-10-03 16:03:37 +0300

    Release v1.0.0

--- src/jmux-go/internal/version/version.go
+++ src/jmux-go/internal/version/version.go
@@ -7,9 +7,9 @@ import (
 
 // Version information - will be set at build time
 var (
-	Version   = "v1.0.0-dev"
-	GitCommit = "unknown"
-	BuildTime = "unknown"
+	Version   = "v1.0.0"
+	GitCommit = "ec08522ac469acae162aca2263fe015a7491a7ec"
+	BuildTime = "2025-10-03 13:03:14 UTC"
 	GoVersion = runtime.Version()
 )
 
@@ -20,7 +20,7 @@ func GetVersion() string {
 
 // GetFullVersion returns detailed version information
 func GetFullVersion() string {
-	return fmt.Sprintf("dmux %s\nGit commit: %s\nBuild time: %s\nGo version: %s",
+	return fmt.Sprintf("dmux %s\\nGit commit: %s\\nBuild time: %s\\nGo version: %s",
 		Version, GitCommit, BuildTime, GoVersion)
 }
 
