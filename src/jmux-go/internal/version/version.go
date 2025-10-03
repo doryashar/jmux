322c0103f2d96ae1195ada3b52f0ce3af0aa0e2f
doryashar <dor.yashar@gmail.com>
2025-10-03 16:03:59 +0300

    Bump version to v1.0.1-dev for development

--- src/jmux-go/internal/version/version.go
+++ src/jmux-go/internal/version/version.go
@@ -7,9 +7,9 @@ import (
 
 // Version information - will be set at build time
 var (
-	Version   = "v1.0.0"
-	GitCommit = "ec08522ac469acae162aca2263fe015a7491a7ec"
-	BuildTime = "2025-10-03 13:03:14 UTC"
+	Version   = "v1.0.1-dev"
+	GitCommit = "unknown"
+	BuildTime = "unknown"
 	GoVersion = runtime.Version()
 )
 
